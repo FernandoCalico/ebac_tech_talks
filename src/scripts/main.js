d283a56c65034eb297201d281dfb27ad3a43a855
Fernando Caliço <fernando.carvalho.calico@gmail.com>
2023-05-06 16:06:37 -0300

    Projeto Concluído - Build

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,3 @@
-// AOS.init();
-
-// const dataEvento = new Date('Dec 12, 2023 19:00:00');
-// const timeStampEvento = dataEvento.getTime();
-
-// const contaTempo = setInterval(function() {
-//     const agora = new Date();
-//     const timeStampAtual = agora.getTime();
-
-//     const tempoAteEvento = timeStampEvento - timeStampAtual;
-//     const diasMs = 1000 * 60 * 60 * 24;
-//     const horasMs = 1000 * 60 * 60;
-//     const minutosMs = 1000 * 60;
-
-//     const diasAteEvento = Math.floor(tempoAteEvento/diasMs);
-//     const horasAteEvento = Math.floor((tempoAteEvento % diasMs) / horasMs);
-//     const minutosAteEvento = Math.floor((tempoAteEvento % horasMs) / minutosMs);
-//     const segundosAteEvento = Math.floor((tempoAteEvento % minutosMs) / 1000);
-
-//     document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
-    
-//     if (tempoAteEvento < 0) {
-//         clearInterval(contaTempo);
-//         document.getElementById('contador').innerHTML = 'EVENTO EXPIRADO!';
-//     };
-
-// }, 1000);
-
 AOS.init();
 
 const dataDoEvento = new Date("Jul 01, 2023 16:00:00");
